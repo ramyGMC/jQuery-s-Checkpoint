c5f267a149d01a102c964222ef4f71fbd80f6d03
ramyGMC <44526841+ramyGMC@users.noreply.github.com>
2018-11-10 17:56:27 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,44 @@
+var test = 10; //walla 3andi variables test valeur 10;
+test = 121; // test wala 121 
+test = test + 10; // test = 131 
+// test = test - 10;
+// test = test * 10;
+// test = test / 10;
+// test = test % 10;
+var a ="rami"; // chaine de caractére 
+
+var alpha = 4
+test = test + alpha // 131 + 4 = 135
+alert(test);
+alert("elyes is the best headof leaders ever" + test);
+var name = prompt("what's your name ?");
+alert("marhbe bik " + name);
+var confirmation = confirm("mrigel khouya ?");
+$("#hello").css("color", "pink");    
+document.getElementById("hello").style.frontSize ="40px";
+document.getElementById("image").setAttribute("src","res/Draven.jpg");
+
+function sum(alpha,test)
+{return alpha+test;}
+var a =(3,5)
+var b =(alpha,test)
+var c =(alpha,b)
+document.getElementById("Surprise").innerHTML ="Just lose it";
+function yo(){
+    alert('yo');
+}
+setTimeout(yo,3000);
+
+
+var test= document.getElementById('test');
+var para= document.getElementById('para');
+
+var couleur = $("#para").css("background-color");
+
+function test2(){
+    if(para.style.backgroundColor == "blueviolet"){
+        para.style.backgroundColor = "yellow";
+    } else {
+        para.style.backgroundColor = "blueviolet";
+    }
+}
